91460f9782dbf150397708cd96ffc05c4d96a377
Vicillian <vic.liu101@gmail.com>
2020-02-20 02:31:25 +0000

    Button works

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@
 
 console.log('hi');
 
-$('h1').click(function(){
+$('button').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
+    fetch("https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=puppy")
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log(data);
+      })
 });
