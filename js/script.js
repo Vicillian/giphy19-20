b085f235f097dcf4c5bcced7b816dd23348e1bcc
Vicillian <vic.liu101@gmail.com>
2020-02-27 02:33:20 +0000

    Buttons work. Will display gif. But only if the search term is a single word

--- js/script.js
+++ js/script.js
@@ -3,13 +3,17 @@
 console.log('hi');
 
 $('button').click(function(){
+  var searchTerm = $("input").val();
+  var encodedSearchTerm = encodeURIComponent(searchTerm);
+  console.log(searchTerm);
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
-    fetch("https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=puppy")
+    fetch("https://api.giphy.com/v1/gifs/search?rating=pg&api_key=dc6zaTOxFJmzC&q=" + encodedSearchTerm)
     .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-        console.log(data);
+        var originalLink = data.data[0].images.original.url;
+      $(".result").append(`<img src="${originalLink}">`);
       })
 });
